61d0a12ded80a32c108c710f2a8746a5993a1ace
bagher <palahangmohammadbagher@gmail.com>
2023-07-28 18:17:35 +0430

    added taggit package

--- blog/settings.py
+++ blog/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
     'post.apps.PostConfig',
+
+    # Third Party Packages
+    'taggit'
 ]
 
 MIDDLEWARE = [
